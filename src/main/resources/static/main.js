87f7a4a8db0a4d8180c8991cc9bb5e72631623a7
Tripp Hill <thill@messerfinancial.com>
2022-06-30 13:17:24 -0400

    horrible v1 like implementation

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,6 +2,7 @@ const host = document.location.host
 
 const feedSocket = new WebSocket(`ws://${host}/feed`)
 const promptSocket = new WebSocket(`ws://${host}/prompt`)
+const patchSocket = new WebSocket(`ws://${host}/patch`)
 const lsUsername = localStorage.getItem("username")
 const username = lsUsername != null ? lsUsername : getUsername();
 
@@ -19,24 +20,38 @@ feedSocket.onmessage = (message) => {
     else
         handleError();
 }
+patchSocket.onmessage = (message) => {
+    const resp = JSON.parse(message.data);
+    const oldDiv = document.getElementById(resp.itemTs)
+    oldDiv.replaceWith(buildEntryDiv(resp.newValue))
+}
 function handleError() {
     alert('Error processing your request');
 }
-
-function handleSuccess(message) {
+function patchLike(itemTs) {
+    patchSocket.send(JSON.stringify({itemTs: itemTs, patchAction: 'LIKE'}));
+}
+function buildEntryDiv(message) {
     const div = document.createElement("div")
-    const id = new Date().getMilliseconds().toString() + "-"+message.prompt.replaceAll(" ","_")
+    const id = `${message.ts}`
     div.setAttribute("id", id)
     div.classList.add("feed-entry")
-    document.getElementById("feed").prepend(div)
+    //document.getElementById("feed").prepend(div)
     const h3 = document.createElement("h3")
-    h3.textContent = `"${message.prompt}" - ${message.username} at ${message.ts}`
+    const likeCount = message.likes == null ? 0 : message.likes
+    h3.innerHTML = `"${message.prompt}" - ${message.username} at ${message.ts} - <span onclick="patchLike('${message.ts}')">&#128077; (${likeCount})</span>`
     div.appendChild(h3);
     message.images.forEach(imageString => {
         const img = document.createElement("img")
         img.setAttribute("src", "data:image/png;base64," + imageString)
         div.appendChild(img)
     });
+    return div;
+}
+
+function handleSuccess(message) {
+    const div = buildEntryDiv(message)
+    document.getElementById("feed").prepend(div)
 }
 
 function submitPrompt() {
